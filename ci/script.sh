bc2430c5ae81ea6aa1963369b2b7eb77be680272
Andrew Gallant <jamslam@gmail.com>
2019-01-03 21:45:28 -0500

    run full test suite less frequently

--- ci/script.sh
+++ ci/script.sh
@@ -4,12 +4,15 @@ set -ex
 
 cargo doc --verbose
 cargo build --verbose
-cargo test --verbose
+cargo test --verbose --lib
+cargo test --verbose --doc
 
 cargo doc --verbose --no-default-features
 cargo build --verbose --no-default-features
 
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+    # these tests take forever, so only do them on nightly
+    cargo test --verbose --test default
     # compile benchmarks, but don't run them
     cargo bench --verbose --manifest-path bench/Cargo.toml ////
     # make sure the debug tool builds
