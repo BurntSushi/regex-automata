e3da4de8c336481d754e9fa01b25edd78bfdaae1
James Munns <james.munns@ferrous-systems.com>
2019-01-09 23:14:15 +0100

    Only test if target supports embedded targets

--- ci/script.sh
+++ ci/script.sh
@@ -10,9 +10,13 @@ cargo test --verbose --doc
 cargo doc --verbose --no-default-features
 cargo build --verbose --no-default-features
 
-# Validate no_std status
-rustup target add thumbv7em-none-eabihf
-cargo build --verbose --no-default-features --target thumbv7em-none-eabihf
+# Validate no_std status if this version of rust supports
+#   embedded targets (starting with 1.31 stable)
+if rustup target add thumbv7em-none-eabihf ; then
+    cargo build --verbose --no-default-features --target thumbv7em-none-eabihf
+else
+    echo "Skipping no_std test..."
+fi
 
 if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
     # these tests take forever, so only do them on nightly
