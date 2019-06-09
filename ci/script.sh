c7c27fbc9ae6ecffb0103773950426cc33aa991b
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2019-06-09 10:43:54 +0200

    deps: bump serde_derive to 0.11
    And also stop running tests on the MSRV.
    
    Closes #3

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,22 @@ set -ex
 
 cargo doc --verbose
 cargo build --verbose
+
+# Our dev dependencies are increasing their MSRV more quickly then we want to,
+# so only test the basic build on non-stable/beta/nightly builds.
+if ! echo "$TRAVIS_RUST_VERSION" | grep -Eq '^[^0-9]+$'; then
+    exit 0
+fi
+
 cargo test --verbose --lib
 cargo test --verbose --doc
 
 cargo doc --verbose --no-default-features
 cargo build --verbose --no-default-features
 
-# Validate no_std status if this version of rust supports
-#   embedded targets (starting with 1.31 stable)
-if rustup target add thumbv7em-none-eabihf ; then
+# Validate no_std status if this version of rust supports embedded targets
+# (starting with 1.31 stable)
+if rustup target add thumbv7em-none-eabihf; then
     cargo build --verbose --no-default-features --target thumbv7em-none-eabihf
 else
     echo "Skipping no_std test..."
