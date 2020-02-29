ef7f6c7b8d9cad416613d79201b335addb89de95
Andrew Gallant <jamslam@gmail.com>
2020-02-29 13:48:15 -0500

    nfa: improve repetition compilation
    This improves the byte code for expressions like 'a{2,5}' in a way
    that makes episilon closures a bit smaller. This makes traversing the
    NFA faster, particularly if it's used directly for matching.

--- src/nfa.rs
+++ src/nfa.rs
@@ -537,11 +537,51 @@ impl NFACompiler {
             return Ok(prefix);
         }
 
-        let suffix = self.compile_concat(
-            (min..max).map(|_| self.compile_zero_or_one(expr, greedy)),
-        )?;
-        self.patch(prefix.end, suffix.start);
-        Ok(ThompsonRef { start: prefix.start, end: suffix.end })
+        // It is tempting here to compile the rest here as a concatenation
+        // of zero-or-one matches. i.e., for `a{2,5}`, compile it as if it
+        // were `aaa?a?a?`. The problem here is that it leads to this program:
+        //
+        //     >000000: 61 => 01
+        //     000001: 61 => 02
+        //     000002: alt(03, 04)
+        //     000003: 61 => 04
+        //     000004: alt(05, 06)
+        //     000005: 61 => 06
+        //     000006: alt(07, 08)
+        //     000007: 61 => 08
+        //     000008: MATCH
+        //
+        // And effectively, once you hit state 2, the epsilon closure will
+        // include states 3, 5, 5, 6, 7 and 8, which is quite a bit. It is
+        // better to instead compile it like so:
+        //
+        //     >000000: 61 => 01
+        //      000001: 61 => 02
+        //      000002: alt(03, 08)
+        //      000003: 61 => 04
+        //      000004: alt(05, 08)
+        //      000005: 61 => 06
+        //      000006: alt(07, 08)
+        //      000007: 61 => 08
+        //      000008: MATCH
+        //
+        // So that the epsilon closure of state 2 is now just 3 and 8.
+        let empty = self.add_empty();
+        let mut prev_end = prefix.end;
+        for _ in min..max {
+            let union = if greedy {
+                self.add_union()
+            } else {
+                self.add_reverse_union()
+            };
+            let compiled = self.compile(expr)?;
+            self.patch(prev_end, union);
+            self.patch(union, compiled.start);
+            self.patch(union, empty);
+            prev_end = compiled.end;
+        }
+        self.patch(prev_end, empty);
+        Ok(ThompsonRef { start: prefix.start, end: empty })
     }
 
     fn compile_at_least(
