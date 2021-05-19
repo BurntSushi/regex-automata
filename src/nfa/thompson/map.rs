f2053eec3766a2be69907ca583660741e3025aad
Andrew Gallant <jamslam@gmail.com>
2021-05-18 21:06:59 -0400

    progress

--- src/nfa/thompson/map.rs
+++ src/nfa/thompson/map.rs
@@ -124,6 +124,9 @@ impl Utf8BoundedMap {
             self.map = vec![Utf8BoundedEntry::default(); self.capacity];
         } else {
             self.version = self.version.wrapping_add(1);
+            // If we loop back to version 0, then we forcefully clear the
+            // entire map. Otherwise, it might be possible to incorrectly
+            // match entries used to generate other NFAs.
             if self.version == 0 {
                 self.map = vec![Utf8BoundedEntry::default(); self.capacity];
             }
