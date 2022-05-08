10b04df3dcbac67a0cc8a155834996f086150cf7
Andrew Gallant <jamslam@gmail.com>
2022-05-07 21:45:48 -0400

    progress

--- src/nfa/thompson/pikevm/regex.rs
+++ src/nfa/thompson/pikevm/regex.rs
@@ -1,3 +1,10 @@
 /*!
 TODO
 */
+
+// BREADCRUMBS: Build the higher level regex API here. Move the iterators
+// from vm.rs to here. We'll need another layer of Config/Builder. Then strip
+// down vm.rs to just its basic find routines without any iterators. Other
+// than some vague high vs low level difference, the regex API will permit
+// bundling a prefilter where as the VM layer will require explicitly providing
+// a prefilter scanner to each search call.
