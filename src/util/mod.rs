d6d96af2abfca42d41f04c6e71658cc706635904
Andrew Gallant <jamslam@gmail.com>
2022-05-13 11:39:26 -0400

    progress

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,17 +6,18 @@ TODO
 use alloc::vec::Vec;
 
 pub mod alphabet;
+pub mod id;
+pub mod iter;
+pub mod prefilter;
+
 pub(crate) mod bytes;
 #[cfg(feature = "alloc")]
 pub(crate) mod determinize;
 pub(crate) mod escape;
-pub mod id;
-pub mod iter;
 #[cfg(feature = "alloc")]
 pub(crate) mod lazy;
 pub(crate) mod matchtypes;
 pub(crate) mod nonmax;
-pub mod prefilter;
 #[cfg(feature = "alloc")]
 pub(crate) mod sparse_set;
 pub(crate) mod start;
