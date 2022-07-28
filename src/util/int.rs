cc128cd47ab05411d20630c2c24541a863ac25b9
Andrew Gallant <jamslam@gmail.com>
2022-07-28 10:28:30 -0400

    progress

--- src/util/int.rs
+++ src/util/int.rs
@@ -1,3 +1,46 @@
+/*!
+This module provides several integer oriented traits for converting between
+both fixed size integers and integers whose size varies based on the target
+(like `usize`).
+
+The driving design principle of this module is to attempt to centralize as many
+`as` casts as possible here. And in particular, we separate casts into two
+buckets:
+
+* Casts that we use for their truncating behavior. In this case, we use more
+descriptive names, like `low_u32` and `high_u32`. * Casts that we use for
+converting back-and-forth between `usize`. These conversions are generally
+necessary because we often store indices in different formats to save on
+memory, which requires converting to and from `usize`. In this case, we very
+specifically do not want to overflow, and so the methods defined here will
+panic if the `as` cast would be lossy in debug mode. (A normal `as` cast will
+never panic!)
+
+For `as` casts between raw pointers, we use `cast`, so `as` isn't needed there.
+
+For regex engines, floating point is just never used, so we don't have to worry
+about `as` casts for those.
+
+Otherwise, this module pretty much covers all of our `as` needs except for one
+thing: const contexts. There are a select few places in this crate where we
+still need to use `as` because const functions on traits aren't stable yet.
+If we wind up significantly expanding our const footprint in this crate, it
+might be worth defining free functions to handle those cases. But at the time
+of writing, that just seemed like too much ceremony. Instead, I comment each
+such use of `as` in a const context with a FIXME notice.
+
+NOTE: for simplicity, we don't talk target pointer width into account here for
+`usize` conversions. Since we currently only panic in debug mode, skipping the
+check when it can be proven it isn't needed at compile time doesn't really
+matter. Now, if we wind up wanting to do as many checks as possible in release
+mode, then we would want to skip those when we know the conversions are always
+lossy.
+
+NOTE: this module isn't an exhaustive API. For example, we still use things
+like `u64::from` where possible, or even `usize::try_from()` we when do
+explicitly want to panic or when we want to return an error for overflow.
+*/
+
 pub(crate) trait U8 {
     fn as_usize(self) -> usize;
 }
@@ -176,6 +219,17 @@ impl Usize for usize {
         }
     }
 }
+
+// Pointers aren't integers, but we convert pointers to integers to perform
+// offset arithmetic in some places. (And no, we don't convert the integers
+// back to pointers.) So add 'as_usize' conversions here too for completeness.
+//
+// These 'as' casts are actually okay because there always non-lossy. But the
+// idea here is to just try and remove as much 'as' as possible, particularly
+// in this crate where we are being really paranoid about offsets and making
+// sure we don't panic on inputs that might be untrusted. This way, the 'as'
+// casts become easier to audit if they're all in one place, even when some of
+// them are actually okay 100% of the time.
 
 pub(crate) trait Pointer {
     fn as_usize(self) -> usize;
