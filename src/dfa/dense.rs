3a76fac237cecfa0c185417d674f23a1de918362
Andrew Gallant <jamslam@gmail.com>
2023-03-09 21:57:40 -0500

    progress

--- src/dfa/dense.rs
+++ src/dfa/dense.rs
@@ -197,7 +197,8 @@ impl Config {
     pub fn prefilter(mut self, pre: Option<Prefilter>) -> Config {
         self.pre = Some(pre);
         if self.specialize_start_states.is_none() {
-            self.specialize_start_states = Some(self.pre.is_some());
+            self.specialize_start_states =
+                Some(self.get_prefilter().is_some());
         }
         self
     }
